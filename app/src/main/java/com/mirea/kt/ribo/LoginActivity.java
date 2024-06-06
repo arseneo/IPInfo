f08b4da6d7e16dc8d87d24a4947985b3e1a033ea
Dragonmaw <arseneo@mail.ru>
2024-06-07 01:22:52 +0300

    Finalizing

--- app/src/main/java/com/mirea/kt/ribo/LoginActivity.java
+++ app/src/main/java/com/mirea/kt/ribo/LoginActivity.java
@@ -2,6 +2,7 @@ package com.mirea.kt.ribo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -88,6 +89,9 @@ public class LoginActivity extends AppCompatActivity {
                             }
                         });
                     } else {
+                        Log.i("task", "Title: "+jsonObject.getString("title"));
+                        Log.i("task", "Task: "+jsonObject.getString("task"));
+                        Log.i("task", "Variant: "+jsonObject.getString("variant"));
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
